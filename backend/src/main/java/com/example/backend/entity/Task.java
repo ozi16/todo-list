edc91d50b664b1d279f6765a8a1071bb49f63170
radhaaaar <radhikarangga14@gmail.com>
2025-09-02 14:40:29 +0700

    nambah crud

--- backend/src/main/java/com/example/backend/entity/Task.java
+++ backend/src/main/java/com/example/backend/entity/Task.java
@@ -1,14 +1,24 @@
 package com.example.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
-    private Integer id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String deskripsi;
-    private Boolean status;
+    private boolean status;
     private String title;
 
 
