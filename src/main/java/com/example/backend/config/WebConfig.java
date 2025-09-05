d6274f2a79c68fefbf7133df116798b40e4e780c
radhaaaar <radhikarangga14@gmail.com>
2025-09-05 21:21:55 +0700

    tambah

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//ini config
 @Configuration
 public class WebConfig {
     @Bean
