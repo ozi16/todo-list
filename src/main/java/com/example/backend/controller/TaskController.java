6a1abc0673308c94929d3b8a751bc46636d40323
radhaaaar <radhikarangga14@gmail.com>
2025-09-09 15:05:05 +0700

    bismilah2

--- src/main/java/com/example/backend/controller/TaskController.java
+++ src/main/java/com/example/backend/controller/TaskController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 // inicontroller 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RequestMapping("/api/")
 @RestController
 public class TaskController {
